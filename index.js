a5ca6124b9f40fcb50438209920ca9061b662893
mashigovincent01 <mashigovincent407@gmail.com>
2024-09-16 20:26:12 +0200

    add condition section and days section add images for the weather

---
+++ index.js
@@ -0,0 +1,7 @@
+
+
+  async function getCats() {
+    //const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/south%20africa?unitGroup=metric&key=6XQQPXKMTYUZP2TS7V9CWW2N2&contentType=json`, {mode: 'cors'});
+    console.log(response);
+  }
+  //getCats();
